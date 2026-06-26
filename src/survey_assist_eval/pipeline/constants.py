6d4808df8637de91e5f633f8f63511a3cc9f442b
peter-spencer-ons <peter.spencer@ext.ons.gov.uk>
2026-06-26 16:10:11 +0000

    chore add level of education names expansion

---
+++ src/survey_assist_eval/pipeline/constants.py
@@ -0,0 +1,15 @@
+# pylint: disable=C0301
+
+"""Contains constants used in pipeline."""
+
+LEVEL_OF_EDUCATION = {
+    "0": "No qualifications",
+    "1": "Level 1 and entry-level qualifications: 1-4 GCSEs (grades A*-C / 9-4), any GCSEs at other grades, O Levels or CSEs (any grade), 1 AS Level, NVQ Level 1, Foundation GNVQ, or Basic/Essential Skills",
+    "2": "Level 2 qualifications: 5+ GCSEs (A*-C / 9-4), O Levels (pass), CSEs (grade 1), School Certificate, 1 A Level or 2-3 AS Levels, VCEs, Intermediate/Higher Diploma, Welsh Baccalaureate (Intermediate), NVQ Level 2, Intermediate GNVQ, City & Guilds Craft, BTEC First/General Diploma, or RSA Diploma",
+    "3": "Apprenticeship",
+    "4": "Level 3 qualifications: 2+ A Levels or VCEs, 4+ AS Levels, Higher School Certificate, Progression/Advanced Diploma, Welsh Baccalaureate (Advanced), NVQ Level 3, Advanced GNVQ, City & Guilds Advanced Craft, ONC/OND, BTEC National, or RSA Advanced Diploma",
+    "5": "Level 4 qualifications or above: Degree (BA, BSc), postgraduate degree (MA, PhD, PGCE), NVQ Levels 4-5, HNC/HND, RSA Higher Diploma, BTEC Higher Level, or professional qualifications (e.g. teaching, nursing, accountancy)",
+    "6": "Other: vocational or work-related qualifications, other qualifications achieved in England or Wales, qualifications achieved outside England or Wales (equivalent not stated or unknown)",
+    "7": "No education information provided: respondent did not provide their level of education",
+    "-8": "Does not apply: Students and schoolchildren living away during term-time, and children aged 15 years and under",
+}
