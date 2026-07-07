a67a4d5c35281c6f3dfd73c91cf944005eda3fec
Ethan Moss <ethan.moss@ons.gov.uk>
2026-07-07 13:52:57 +0100

    feat: added eval metric and perf calculation stages

--- scripts/soc_pipeline/api_e2e.py
+++ scripts/soc_pipeline/api_e2e.py
@@ -23,6 +23,8 @@ from survey_assist_eval.pipeline.api.core import (
     ApiEvaluatorConfig,
 )
 from survey_assist_eval.pipeline.api.data import (
+    calc_eval_metrics,
+    calc_eval_perf,
     get_and_prepare_test_data,
     prep_data_for_classify,
     prep_data_for_lookup,
@@ -110,23 +112,25 @@ def main(classify_type: Literal["sic", "soc"]) -> None:
     )
     df = record_classify_results(df, classify_ids, classify_responses)
 
-    # TODO: update with results of metrics calculation once implemented
     logger.info("Calculating evaluation metrics...")
-    metrics = {}
+    eval_metrics = calc_eval_metrics(df, classify_type, log_level=LOG_LEVEL)
+    logger.info("Evaluation metrics calculated successfully.")
 
-    # record for evaluation metadata purposes
+    # calculate performance metrics for evaluation metadata purposes
     end_time = datetime.datetime.now(tz=datetime.UTC)
-    duration = (end_time - start_time).total_seconds()
-    logger.info(f"API evaluation completed in {duration}s.")
+    perf_metrics = calc_eval_perf(df, start_time, end_time)
+    logger.info(
+        f"API evaluation completed in {perf_metrics['duration_seconds']}s."
+    )
 
     # write evaluation results to firestore for future analysis
     logger.info("Storing evaluation results in firestore...")
     api_evaluator.store_eval_results(
         start_time,
         end_time,
-        duration,
         api_config,
-        metrics,
+        eval_metrics,
+        perf_metrics,
     )
     logger.info("Evaluation results stored successfully.")
 
