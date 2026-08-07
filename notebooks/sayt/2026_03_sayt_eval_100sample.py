5c6856ecd0a7ee8dca153cbd4aff9a222b789277
Peter Spencer <peter.spencer@ext.ons.gov.uk>
2026-08-07 13:17:43 +0100

    SA787 - SAYT metrics (#38)
    * feat: SAYT analysis - prepare main functions, make them robust; update sayt_eval notebook to use neew functions
    
    * chore: correct docstrings
    
    * chore: adapt methods to use in merged files (SA837)
    
    * chore: typos
    
    * chore: generalise melt method for wider use
    
    * chore: correct the time output to capture each run of the loop
    
    * chore: uncomment suggesters
    
    * chore: include elapsed time in loggs
    
    * chore: rename method for clarity
    
    * chore: correct docstrings
    
    * retrigger checks
    
    * retrigger checks

--- notebooks/sayt/2026_03_sayt_eval_100sample.py
+++ notebooks/sayt/2026_03_sayt_eval_100sample.py
@@ -1,3 +1,4 @@
+# %%
 """Run small tests for industry/organisation descriptions SAYT.
 
 Expects following environment variables to be set:
@@ -10,10 +11,8 @@ The variables are loaded from the ".env" file.
 # %%
 import logging
 import os
-import time
 
 import pandas as pd
-import plotly.express as px
 from dotenv import load_dotenv
 from survey_assist_embed_core.sayt import (
     NgramRetrieverSpec,
@@ -25,8 +24,9 @@ from survey_assist_utils.logging import get_logger
 
 from notebooks.sayt.sayt_utils import (
     build_lookup_suggester,
-    get_suggestions_for_row,
-    rank_of_correct_code_in_suggestions,
+    create_figure,
+    get_suggestions_by_chars,
+    melt_results_for_analysis,
     validate_one_code,
 )
 
@@ -49,7 +49,6 @@ if not os.path.exists(OUTPUT_DIR):
 
 logger = get_logger(__name__)
 logger.info("Location specs", bucket_name=bucket_name, output_dir=OUTPUT_DIR)
-
 
 # %%
 test_df = pd.read_excel(
@@ -77,14 +76,12 @@ for col in [
         test_df[col].replace({"5 or 12": "5"}), errors="coerce"
     )
 
-
 # %%
 # check the codes are well formed
 print(
     f'Clerical codes validated: {
         test_df["correct_sic_code"]
         .apply(validate_one_code,
-               logger=logger,
                code_length=SIC_CODE_LENGTH)
                .all()
                }'
@@ -158,101 +155,14 @@ if EXTENDED_RUN:
         }
     )
 
+# %%
+suggestions_df = get_suggestions_by_chars(test_df, suggesters_dict=suggesters)[0]
 
 # %%
-
-for prefix_chars in [4, 5, 7, 10]:  # 150]:
-    for suggester_name, suggester_obj in suggesters.items():
-        logger.info(
-            "Starting SAYT suggesting - one loop",
-            num_chars=prefix_chars,
-            suggester_label=suggester_name,
-        )
-
-        t_start = time.perf_counter()
-        test_df[f"suggestions_{prefix_chars}chars_{suggester_name}"] = test_df.apply(
-            get_suggestions_for_row,
-            suggester=suggester_obj,
-            max_suggestions=MAX_SUGGESTIONS,
-            num_chars=prefix_chars,
-            axis=1,
-        )
-        elapsed = time.perf_counter() - t_start
-        logger.info(
-            "  -> suggestions done",
-            elapsed_sec=elapsed,
-            elapsed_per_row_ms=elapsed / len(test_df) * 1000,
-        )
-        test_df[f"rank_{prefix_chars}chars_{suggester_name}"] = test_df.apply(
-            rank_of_correct_code_in_suggestions,
-            correct_code_col="correct_sic_code",
-            suggester_label=suggester_name,
-            num_chars=prefix_chars,
-            code_length=SIC_CODE_LENGTH,
-            axis=1,
-        )
+melt_df = melt_results_for_analysis(df=suggestions_df)
 
 # %%
-# melt results by suggester and num_chars for easier analysis
-results_df = test_df.melt(
-    id_vars=["correct_sic_code", "full_entry"],
-    value_vars=[col for col in test_df.columns if col.startswith("rank_")],
-    var_name="suggester_numchars",
-    value_name="rank",
-)
-results_df["num_chars"] = results_df["suggester_numchars"].apply(
-    lambda x: int(x.split("_")[1].replace("chars", ""))
-)
-results_df["suggester"] = results_df["suggester_numchars"].apply(
-    lambda x: " ".join(x.split("_")[2:])
-)
-results_df.loc[results_df["rank"] > MAX_SUGGESTIONS, "rank"] = None
-results_df["rank"] = results_df["rank"].fillna(
-    MAX_SUGGESTIONS + 2
-)  # Treat not found as worst rank
-results_df["rank"] = results_df["rank"].astype(int)
-
-results_df = results_df.sort_values(by=["num_chars", "suggester", "rank"]).reset_index(
-    drop=True
-)
-
-# %%
-# compare rank histograms for the two suggesters at different num_chars
-fig = px.histogram(
-    results_df,
-    x="rank",
-    color="suggester",
-    facet_col="num_chars",
-    category_orders={
-        "rank": list(range(0, MAX_SUGGESTIONS + 2)),
-        "suggester": sorted(results_df["suggester"].unique().tolist()),
-    },
-    barmode="group",
-    title=(
-        "Distribution of Ranks of Correct Code in Suggestions by Number of Characters"
-        + " (on SAYT test data of 100 examples)"
-    ),
-)
-fig.update_xaxes(
-    tickmode="array",
-    tickvals=[*range(1, MAX_SUGGESTIONS + 1, 1), MAX_SUGGESTIONS + 2],
-    ticktext=[str(i) for i in range(1, MAX_SUGGESTIONS + 1, 1)] + ["NA"],
-)
-
-fig.update_layout(
-    bargap=0.1,
-    legend={
-        "title": "Suggester method",
-        "orientation": "h",
-        "yanchor": "top",
-        "y": -0.2,
-        "xanchor": "center",
-        "x": 0.5,
-    },
-)
-fig.show()
-
-fig.write_html(f"{OUTPUT_DIR}/sayt_eval_100sample_rank_histograms.html")
+create_figure(melt_df, output_dir=OUTPUT_DIR)
 
 # %%
 # log the dimensions of the retriever index matrices for each retriever
@@ -278,6 +188,3 @@ for suggester_name, suggester_obj in suggesters.items():
             retriever_name=name,
             matrix_shape=shape,
         )
-
-
-# %%
