8d0a3f7a62c75b163222efca78df8a88bb14a906
Peter <peter.spencer@ext.ons.gov.uk>
2026-09-01 16:14:00 +0100

    chore: save files to bucket

--- notebooks/sayt/2026_08_find_best_weights.py
+++ notebooks/sayt/2026_08_find_best_weights.py
@@ -4,6 +4,12 @@
 
 # %%
 import json
+import os
+
+import gcsfs
+from dotenv import load_dotenv
+
+# %%
 
 
 # %%
@@ -27,22 +33,49 @@ def find_best_performing_setup(path: str):
 
 
 # %%
-weights_5 = "notebooks/sayt/weights/weight_5_test_n_p_s.json"
-weights_6 = "notebooks/sayt/weights/weight_6_test_n_p_s.json"
-weights_7 = "notebooks/sayt/weights/weight_7_test_n_p_s.json"
-# weights_8 = "notebooks/sayt/weights/weight_8_test_n_p_s.json"
-# weights_9 = "notebooks/sayt/weights/weight_9_test_n_p_s.json"
+# weights_5 = "notebooks/sayt/weights/weight_5_test_n_p_s.json"
+# weights_6 = "notebooks/sayt/weights/weight_6_test_n_p_s.json"
+# weights_7 = "notebooks/sayt/weights/weight_7_test_n_p_s.json"
+weights_8 = "notebooks/sayt/weights/weight_8_test_n_p_s.json"
+weights_9 = "notebooks/sayt/weights/weight_9_test_n_p_s.json"
 
 # %%
-ms5, bd5 = find_best_performing_setup(weights_5)
-ms6, bd6 = find_best_performing_setup(weights_6)
-ms7, bd7 = find_best_performing_setup(weights_7)
-# ms8, bd8 = find_best_performing_setup(weights_8)
-# ms9, bd9 = find_best_performing_setup(weights_9)
+# ms5, bd5 = find_best_performing_setup(weights_5)
+# ms6, bd6 = find_best_performing_setup(weights_6)
+# ms7, bd7 = find_best_performing_setup(weights_7)
+ms8, bd8 = find_best_performing_setup(weights_8)
+ms9, bd9 = find_best_performing_setup(weights_9)
 
 # %%
-print("5 characters:", ms5, "\n", bd5.keys())
-print("6 characters:", ms6, "\n", bd6.keys())
-print("7 characters:", ms7, "\n", bd7.keys())
-# print("8 characters:", ms8, "\n", bd8.keys())
-# print("9 characters:", ms9, "\n", bd9.keys())
+# print("5 characters:", ms5, "\n", bd5.keys())
+# print("6 characters:", ms6, "\n", bd6.keys())
+# print("7 characters:", ms7, "\n", bd7.keys())
+print("8 characters:", ms8, "\n", bd8.keys())
+print("9 characters:", ms9, "\n", bd9.keys())
+
+# %%
+load_dotenv()
+bucket_name = os.getenv("EVALUATION_BUCKET_NAME")
+if not bucket_name:
+    raise ValueError("EVALUATION_BUCKET_NAME environment variable not set")
+
+
+# %%
+characters = [8, 9]
+
+# %%
+fs = gcsfs.GCSFileSystem()
+
+# %%
+for character in characters:
+    save_path = f"gs://{bucket_name}/evaluation-pipeline/SAYT/weights_by_character/weight_{character}_test_n_p_s.json"
+
+    with open(
+        f"notebooks/sayt/weights/weight_{character}_test_n_p_s.json", encoding="utf-8"
+    ) as f:
+        file = json.load(f)
+
+    with fs.open(save_path, "w", encoding="utf-8") as f:
+        json.dump(file, f, ensure_ascii=False, indent=4)
+
+    print(f"Saved dictionary to: {save_path}")
