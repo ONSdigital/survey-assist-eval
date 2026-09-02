b50aae7f51352b89787ed911224291f1908f4194
Peter <peter.spencer@ext.ons.gov.uk>
2026-09-02 14:52:36 +0100

    chore: decrease grid test, save files to the bucket

--- notebooks/sayt/2026_08_sayt_eval_weights.py
+++ notebooks/sayt/2026_08_sayt_eval_weights.py
@@ -2,13 +2,13 @@
 # pylint: disable=R0801, C0103
 """Check the interactions between weights used for different suggesters."""
 
-import json
-
 # %%
+import json
 import os
 
 import pandas as pd
 from dotenv import load_dotenv
+from google.cloud import storage as gcs
 from survey_assist_embed_core.sayt import (
     NgramRetrieverSpec,
     PrefixRetrieverSpec,
@@ -25,7 +25,9 @@ from notebooks.sayt.suggester_eval import run_eval_for_suggesters
 SIC_CODE_LENGTH = 5
 MAX_SUGGESTIONS = 9
 CORRECT_CODE_COL = "correct_sic_code"
-NUM_CHARACTERS_LIST = range(5, 10)
+NUM_CHARACTERS_LIST = range(4, 10)
+
+GRID_GRANULARITY = 10
 
 # %%
 load_dotenv()
@@ -33,12 +35,15 @@ bucket_name = os.getenv("EVALUATION_BUCKET_NAME")
 if not bucket_name:
     raise ValueError("EVALUATION_BUCKET_NAME environment variable not set")
 
-OUTPUT_DIR = "data/figures/sayt/min_characters"
+OUTPUT_DIR = "data/figures/sayt/character_weights"
 if not os.path.exists(OUTPUT_DIR):
     os.makedirs(OUTPUT_DIR)
 
 logger = get_logger(__name__)
 logger.info("Location specs", bucket_name=bucket_name, output_dir=OUTPUT_DIR)
+
+client = gcs.Client()
+blob_name = "evaluation-pipeline/SAYT/weights_by_character/"
 
 # %%
 test_df = pd.read_excel(
@@ -79,7 +84,7 @@ sayt_corpus = list(
 )
 
 # %%
-folder = "notebooks/sayt/weights"
+folder = "notebooks/sayt/weights_sum_10"
 
 for characters in NUM_CHARACTERS_LIST:
 
@@ -88,82 +93,98 @@ for characters in NUM_CHARACTERS_LIST:
     if os.path.exists(main_file_name):
         print(f"File {main_file_name} already exists.")
         continue
-    for ngram in range(1, 11):
-        for prefix in range(1, 11):
-            for semantic in range(1, 11):
+    for ngram in range(0, GRID_GRANULARITY + 1):
+        for prefix in range(0, GRID_GRANULARITY + 1 - ngram):
+            semantic = GRID_GRANULARITY - ngram - prefix
 
-                sub_file_name = (
-                    f"{folder}/w_{characters}_n{ngram}_p{prefix}_s{semantic}.json"
-                )
+            sub_file_name = (
+                f"{folder}/w_{characters}_n{ngram}_p{prefix}_s{semantic}.json"
+            )
 
-                if os.path.exists(sub_file_name):
-                    continue
-                suggesters_three = {
-                    "ngram, prefix and semantic": build_lookup_suggester(
-                        sayt_corpus,
-                        retrievers=[
-                            NgramRetrieverSpec(weight=ngram / 10),
-                            PrefixRetrieverSpec(weight=prefix / 10),
-                            SemanticRetrieverSpec(weight=semantic / 10),
-                        ],
-                    ),
-                }
+            if os.path.exists(sub_file_name):
+                continue
 
-                suggestions_df, fig, metrics_table = run_eval_for_suggesters(
-                    df=test_df,
-                    suggesters_dict=suggesters_three,
-                    num_chars=[characters],
-                    suggestions_limit=MAX_SUGGESTIONS,
-                    correct_code_col=CORRECT_CODE_COL,
-                    output_dir=f"{OUTPUT_DIR}_all",
-                )
+            retrievers_list = []
+            if ngram > 0:
+                retrievers_list.append(NgramRetrieverSpec(weight=ngram))
+            if prefix > 0:
+                retrievers_list.append(PrefixRetrieverSpec(weight=prefix))
+            if semantic > 0:
+                retrievers_list.append(SemanticRetrieverSpec(weight=semantic))
 
-                data = {
-                    "Ngram_weight": ngram,
-                    "Prefix_weight": prefix,
-                    "Semantic_weight": semantic,
-                    "MRR": metrics_table["mrr"][0],
-                    "avg_time": metrics_table["ave_time_per_query_ms"][0],
-                }
-                print(data)
+            suggesters_three = {
+                "ngram, prefix and semantic": build_lookup_suggester(
+                    sayt_corpus,
+                    retrievers=retrievers_list,
+                ),
+            }
 
-                with open(sub_file_name, "w", encoding="utf-8") as f:
-                    json.dump(data, f, indent=4)
+            suggestions_df, fig, metrics_table = run_eval_for_suggesters(
+                df=test_df,
+                suggesters_dict=suggesters_three,
+                num_chars=[characters],
+                suggestions_limit=MAX_SUGGESTIONS,
+                correct_code_col=CORRECT_CODE_COL,
+                output_dir=f"{OUTPUT_DIR}_all",
+            )
+
+            data = {
+                "Ngram_weight": ngram,
+                "Prefix_weight": prefix,
+                "Semantic_weight": semantic,
+                "MRR": metrics_table["mrr"][0],
+                "avg_time": metrics_table["ave_time_per_query_ms"][0],
+            }
+            print(data)
+
+            with open(sub_file_name, "w", encoding="utf-8") as f:
+                json.dump(data, f, indent=4)
 
 # %%
 # combine separate test results into one file
 
-folderpath = "notebooks/sayt/weights"
-remove_files = True
+remove_files = False
 
-master_dict = {}
-files_to_delete = []
-character_file = 8
-main_file_name = f"{folder}/weight_{character_file}_test_n_p_s.json"
+for character_file in NUM_CHARACTERS_LIST:
+    master_dict = {}
+    files_to_delete = []
+    final_file_name = f"weight_{character_file}_test_n_p_s.json"
+    main_file_name = f"{folder}/{final_file_name}"
 
-if os.path.exists(main_file_name):
-    print("Final file already exists.")
-else:
-    for filename in sorted(os.listdir(folderpath)):
-        if filename.startswith(f"w_{character_file}_n") and filename.endswith(".json"):
-            full_path = os.path.join(folderpath, filename)
-            key_name = filename[:-5]  # remove .json from the file name
-            test_name = f"test{key_name[3:]}"
-            with open(full_path, encoding="utf-8") as f:
-                master_dict[test_name] = json.load(f)
-            files_to_delete.append(full_path)
+    if os.path.exists(main_file_name):
+        print("Final file already exists.")
+    else:
+        for filename in sorted(os.listdir(folder)):
+            if filename.startswith(f"w_{character_file}_n") and filename.endswith(
+                ".json"
+            ):
+                full_path = os.path.join(folder, filename)
+                key_name = filename[:-5]  # remove .json from the file name
+                test_name = f"test{key_name[3:]}"
+                with open(full_path, encoding="utf-8") as f:
+                    master_dict[test_name] = json.load(f)
+                files_to_delete.append(full_path)
+        # Save locally
+        with open(
+            os.path.join(folder, final_file_name),
+            "w",
+            encoding="utf-8",
+        ) as f:
+            json.dump(master_dict, f, indent=4)
 
-    with open(
-        os.path.join(folderpath, f"weight_{character_file}_test_n_p_s.json"),
-        "w",
-        encoding="utf-8",
-    ) as f:
-        json.dump(master_dict, f, indent=4)
+        print(f"File {final_file_name} saved.")
+        # Save to the bucket
+        blob = client.bucket(bucket_name).blob(blob_name + final_file_name)
+        blob.upload_from_string(
+            json.dumps(final_file_name, indent=4), content_type="application/json"
+        )
 
-# remove files
-if remove_files:
-    for file_path in files_to_delete:
-        os.remove(file_path)
-    print("Source files removed.")
-else:
-    print("Source files not removed.")
+    # remove files
+    if remove_files:
+        for file_path in files_to_delete:
+            os.remove(file_path)
+        print("Source files removed.")
+    else:
+        print("Source files not removed.")
+
+# %%
