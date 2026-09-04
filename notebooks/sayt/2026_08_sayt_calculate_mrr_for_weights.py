4665983dd6f24379cb57183a93491db68393ae66
Peter <peter.spencer@ext.ons.gov.uk>
2026-09-04 11:15:30 +0100

    chore: change output folder location

--- notebooks/sayt/2026_08_sayt_calculate_mrr_for_weights.py
+++ notebooks/sayt/2026_08_sayt_calculate_mrr_for_weights.py
@@ -29,7 +29,7 @@ CORRECT_CODE_COL = "correct_sic_code"
 NUM_CHARACTERS_LIST = list(range(4, 10))
 
 GRID_GRANULARITY = 10
-FOLDER = "notebooks/sayt/weights_sum_10"
+FOLDER = "data/sayt/weights_sum_10"
 
 # %%
 if not os.path.exists(FOLDER):
