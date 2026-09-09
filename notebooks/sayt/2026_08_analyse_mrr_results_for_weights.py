da32eff20f7d44da6a0335533deebe769a0845af
Peter <peter.spencer@ext.ons.gov.uk>
2026-09-09 16:20:40 +0100

    feat: create heatmap and ternary plot

--- notebooks/sayt/2026_08_analyse_mrr_results_for_weights.py
+++ notebooks/sayt/2026_08_analyse_mrr_results_for_weights.py
@@ -6,13 +6,15 @@
 import json
 import os
 
+import pandas as pd
 import plotly.graph_objects as go
 from dotenv import load_dotenv
 
 from src.survey_assist_eval.pipeline.shared_components import _read_json
 
 # %%
-LOCAL_DIR = "data/sayt/weights_grid_10_lookup_it3/"
+TEST_FOLDER = "weights_grid_10_sic_kb"
+LOCAL_DIR = f"data/sayt/{TEST_FOLDER}/"
 USE_BUCKET = True
 
 # %%
@@ -20,7 +22,7 @@ load_dotenv()
 bucket_name = os.getenv("EVALUATION_BUCKET_NAME")
 if not bucket_name:
     raise ValueError("EVALUATION_BUCKET_NAME environment variable not set")
-blob_name = "evaluation-pipeline/SAYT/weights_by_character/"
+BLOB_NAME = f"evaluation-pipeline/SAYT/weights_by_character/{TEST_FOLDER}/"
 
 
 # %%
@@ -35,9 +37,8 @@ def get_data(
     Args:
         characters (int): The number of characters to consider for the test.
         use_bucket (bool): Whether to read data from a cloud bucket or local file.
-        bucket_path (str, optional): The path to the cloud bucket. Required if use_bucket is True.
-        local_path (str, optional): The path to the local directory.
-            Required if use_bucket is False.
+        bucket_path (str, optional): The path to the cloud bucket. Required if use_bucket==True.
+        local_path (str, optional): The path to the local directory. Required if use_bucket==False.
 
     Returns:
         dict: A dictionary containing the test results with MRR scores.
@@ -45,15 +46,15 @@ def get_data(
     file_name = f"weight_test_{characters}chars_n_p_s.json"
     if use_bucket:
         path = bucket_path + file_name
-        data = _read_json(path)
+        data_file = _read_json(path)
 
     else:
         weights_file = f"{local_path}{file_name}"
 
         with open(weights_file, encoding="utf-8") as f:
-            data = json.load(f)
+            data_file = json.load(f)
 
-    return data
+    return data_file
 
 
 # %%
@@ -96,11 +97,12 @@ def get_ranked_setups(data: dict):
 
 
 # %%
-def generate_ternary_plot(data: dict):
+def generate_ternary_plot(data: dict, character: int):
     """Generate a ternary plot of the n/p/s weight combinations.
 
     Args:
         data (dict): A dictionary containing the test results with MRR scores.
+        character (int): The number of characters to consider for the test.
 
     Returns:
         fig: A Plotly figure object representing the ternary plot.
@@ -142,21 +144,53 @@ def generate_ternary_plot(data: dict):
             marker={
                 "size": 10,
                 "color": [point["mrr"] for point in points],
-                "colorscale": "Viridis",
+                "colorscale": "Jet",
                 "showscale": True,
                 "colorbar": {"title": "MRR score"},
+                "reversescale": True,
             },
         )
     )
 
     fig.update_layout(
-        title="Weight Configurations",
+        title=f"Weight Configurations ({character} characters)",
         ternary={
             "sum": 1,
-            "aaxis": {"title": "ngram"},
-            "baxis": {"title": "prefix"},
-            "caxis": {"title": "semantic"},
+            "aaxis": {"title": ""},
+            "baxis": {"title": ""},
+            "caxis": {"title": ""},
         },
+    )
+
+    fig.add_annotation(
+        text="ngram",
+        x=0.25,
+        y=0.5,
+        xref="paper",
+        yref="paper",
+        textangle=-60,
+        showarrow=False,
+        font={"size": 14},
+    )
+    fig.add_annotation(
+        text="prefix",
+        x=0.5,
+        y=-0.15,
+        xref="paper",
+        yref="paper",
+        textangle=0,
+        showarrow=False,
+        font={"size": 14},
+    )
+    fig.add_annotation(
+        text="semantic",
+        x=0.75,
+        y=0.5,
+        xref="paper",
+        yref="paper",
+        textangle=60,
+        showarrow=False,
+        font={"size": 14},
     )
 
     # fig.show()
@@ -164,29 +198,31 @@ def generate_ternary_plot(data: dict):
 
 
 # %%
+# Best performing setup for each character count
 for i in range(4, 10):
-    data_file = get_data(
+    data_weights = get_data(
         characters=i,
         use_bucket=USE_BUCKET,
-        bucket_path=f"gs://{bucket_name}/{blob_name}",
+        bucket_path=f"gs://{bucket_name}/{BLOB_NAME}",
         local_path=LOCAL_DIR,
     )
 
-    mrr_score, best_dict = find_best_performing_setup(data_file)
+    mrr_score, best_dict = find_best_performing_setup(data_weights)
     print(f"Best MRR for {i} characters: {mrr_score}")
     print(f"Best setup for {i} characters: {best_dict.keys()}\n")
 
 # %%
-character = 5
+# Top 5 performing setups
+char = 9
 
-data_file = get_data(
-    characters=character,
+data_weights = get_data(
+    characters=char,
     use_bucket=USE_BUCKET,
-    bucket_path=f"gs://{bucket_name}/{blob_name}",
+    bucket_path=f"gs://{bucket_name}/{BLOB_NAME}",
     local_path=LOCAL_DIR,
 )
 
-rankings_by_weight = get_ranked_setups(data_file)
+rankings_by_weight = get_ranked_setups(data_weights)
 
 for rank, (individual_score, setups) in enumerate(rankings_by_weight.items(), start=1):
     print(f"Rank {rank}: MRR={individual_score}")
@@ -196,18 +232,72 @@ for rank, (individual_score, setups) in enumerate(rankings_by_weight.items(), st
 
 # %%
 # Access data for visualisation
-character = 7
-for character in range(4, 10):
-    data_file = get_data(
-        characters=character,
+
+characters_list = [6, 9]
+for char in characters_list:
+    data_weights = get_data(
+        characters=char,
         use_bucket=USE_BUCKET,
-        bucket_path=f"gs://{bucket_name}/{blob_name}",
+        bucket_path=f"gs://{bucket_name}/{BLOB_NAME}",
         local_path=LOCAL_DIR,
     )
-    plot = generate_ternary_plot(data_file)
+    plot = generate_ternary_plot(data_weights, char)
     plot.show()
-    plot.write_html(
-        f"data/sayt/weights_grid_10_lookup_it3/ternary_plot_{character}_chars.html"
-    )
+    # plot.write_html(f"data/sayt/{TEST_FOLDER}/ternary_plot_{char}_chars.html")
 
+# %%
+colours = ["Blues"]
+characters_list = [4]
+for char in characters_list:
+    for c in colours:
+        data_weights = get_data(
+            characters=char,
+            use_bucket=USE_BUCKET,
+            bucket_path=f"gs://{bucket_name}/{BLOB_NAME}",
+            local_path=LOCAL_DIR,
+        )
+        df = pd.DataFrame.from_dict(data_weights, orient="index")
+        heatmap_data = df.pivot_table(
+            index="Ngram_weight", columns="Semantic_weight", values="MRR"
+        )
+        semantic_matrix = df.pivot_table(
+            index="Ngram_weight", columns="Semantic_weight", values="Prefix_weight"
+        )
+        x_vals = [val / 10 for val in heatmap_data.columns]
+        y_vals = [val / 10 for val in heatmap_data.index]
+        c_scaled = [
+            [val / 10 if pd.notna(val) else None for val in row]
+            for row in semantic_matrix.values
+        ]
+        text_matrix = [
+            [f"{val*100:.2f}" if pd.notna(val) and val != 0 else "" for val in row]
+            for row in heatmap_data.values
+        ]
+
+        heat_fig = go.Figure(
+            data=go.Heatmap(
+                x=x_vals,
+                y=y_vals,
+                z=heatmap_data.values.tolist(),
+                customdata=c_scaled,
+                text=text_matrix,
+                texttemplate="%{text}",
+                textfont={"size": 10},
+                colorscale=c,
+                colorbar={"title": "MRR"},
+                hovertemplate=(
+                    "Ngram Weight: %{y}<br>"
+                    "Semantic Weight: %{x}<br>"
+                    "Prefix Weight: %{customdata}<br>"
+                    "MRR: %{z}<extra></extra>"
+                ),
+            )
+        )
+        heat_fig.update_layout(
+            title=f"Weight Configurations ({char} characters, colour: {c})",
+            xaxis_title="semantic",
+            yaxis_title="ngram",
+            plot_bgcolor="white",
+        )
+        heat_fig.show()
 # %%
